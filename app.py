cca684379774831110d28f985ee3a2686cb1cee3
Lavanya <lavanyax989@gmail.com>
2026-08-27 05:32:35 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ try:
                     """
                     
                     response = client.models.generate_content(
-                        model="gemini-2.0-flash",
+                        model="gemini-3.6-flash",
                         contents=[image, prompt]
                     )
                     
